c724032204149dd0a40073afc7933eb15b489458
Bob <bob.luursema@outlook.com>
2020-02-22 18:19:05 +0100

    update

--- app_statistieken_analyse.py
+++ app_statistieken_analyse.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from datetime import date
 import time
 
@@ -13,29 +13,11 @@ df['correctie'] = 100 / df['spelers']
 for speler in ['buddy', 'miljuschka', 'nathan', 'leonie', 'tina', 'anita', 'jaike', 'johan', 'claes', 'rob']:
     df['{}_correctie'.format(speler)] = df[speler] - df['correctie']
 
-print(df)
+plotdf = df.drop(labels=['spelers', 'unix', 'buddy','miljuschka','nathan','leonie','tina', 'anita', 'jaike', 'johan', 'claes', 'rob', 'correctie'], axis=1)
 
-sns.set()
+plt.figure()
 
-fig, ax1 = plt.subplots(1, 1)
+ax = plotdf.plot()
+ax.legend(bbox_to_anchor=(0.3, 0.5))
 
-g = sns.lineplot(x='unix', y='buddy_correctie', data=df, label='buddy', ax=ax1)
-sns.lineplot(x='unix', y='miljuschka_correctie', data=df, label='miljuschka', ax=ax1)
-sns.lineplot(x='unix', y='nathan_correctie', data=df, label='nathan', ax=ax1)
-sns.lineplot(x='unix', y='leonie_correctie', data=df, label='leonie', ax=ax1)
-sns.lineplot(x='unix', y='tina_correctie', data=df, label='tina', ax=ax1)
-sns.lineplot(x='unix', y='anita_correctie', data=df, label='anita', ax=ax1)
-sns.lineplot(x='unix', y='jaike_correctie', data=df, label='jaike', ax=ax1)
-sns.lineplot(x='unix', y='johan_correctie', data=df, label='johan', ax=ax1)
-sns.lineplot(x='unix', y='claes_correctie', data=df, label='claes', ax=ax1)
-sns.lineplot(x='unix', y='rob_correctie', data=df, label='rob', ax=ax1)
-
-box = g.get_position()
-g.set_position([box.x0, box.y0, box.width * 0.85, box.height])
-
-plt.xlabel("Unix timestamp")
-plt.ylabel("% stemmen")
-
-plt.legend(bbox_to_anchor=(1.35, 0.5), loc='center right', ncol=1)
-
-plt.show()
+plt.savefig("../storage/pictures/plot.png")
--- poules_analyse.py
+++ poules_analyse.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv('poules.csv')
+
+validate(df)
+
+import os
+os.exit()
+
 df = df.drop("anita", axis=1)
 df = df.drop("tina", axis=1)
 df = df.drop("jaike", axis=1)
 df = df.drop("johan", axis=1)
+df = df.drop("claes", axis=1)
 df = df.drop("aflevering", axis=1)
+
+df = df[df.poule.isin(['all', 'luursema'])]
 
 grouped = df.groupby('speler').sum()
 
 grouped.transpose().plot(kind='bar')
-plt.show()
+plt.savefig('../storage/pictures/poules.png')
+
+def validate(df):
+    for speler in df.speler:
+        print(speler)
